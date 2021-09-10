ea5a3e249afe26a62d04c53f316e6ebb2bb01840
nicolas tanco <nicolastanco001@gmail.com>
2021-09-10 12:23:44 -0300

    update

--- src/App.js
+++ src/App.js
@@ -1,55 +1,56 @@
-import React from "react"
-import data from "./components/data.json"
-import MainText from "./components/MainText"
-import Option from "./components/Option"
-import History from "./components/History"
-
-
+import React from "react";
+import data from "./components/data.json";
+import MainText from "./components/MainText";
+import Option from "./components/Option";
+import History from "./components/History";
 
 class App extends React.Component {
-
-state={
+  state = {
     historias: data,
     bitacora: [],
     counter: 1,
-    currentId: "1"
+    currentId: "1",
+  };
+
+  shouldComponentUpdate() {
+    if (this.state.counter >= 5) return false;
+    else return true;
   }
 
-
- 
-
- shouldComponentUpdate(){
-   if(this.state.counter>=5) return false
-   else return true
- }
-
- handleCounter=(option)=>{
-
-    let newCounter=this.state.counter+1;
+  handleCounter = (option) => {
+    let newCounter = this.state.counter + 1;
     let newBitacora = this.state.bitacora.concat(option);
-    let newCurrentId = newCounter+option.toLowerCase();
-    this.setState({counter:newCounter, bitacora: newBitacora, currentId: newCurrentId});
-    if(newCounter>5) alert("Fin.");
-    
+    let newCurrentId = newCounter + option.toLowerCase();
+    this.setState({
+      counter: newCounter,
+      bitacora: newBitacora,
+      currentId: newCurrentId,
+    });
+    if (newCounter > 5) alert("Fin.");
+  };
 
-  }
-
-  
-  
-  
-  render(){
-    
-  return ( 
-    <div className="App">      
-      <div className="layout">      
-       <MainText  aMostrar= {this.state.historias.find(e=>e.id===this.state.currentId).historia}/>
-       <Option counter={this.handleCounter} opciones={this.state.historias.find(e=>e.id===this.state.currentId).opciones}/>
-       <History bitacora={this.state.bitacora}/> 
+  render() {
+    return (
+      <div className="App">
+        <div className="layout">
+          <MainText
+            aMostrar={
+              this.state.historias.find((e) => e.id === this.state.currentId)
+                .historia
+            }
+          />
+          <Option
+            counter={this.handleCounter}
+            opciones={
+              this.state.historias.find((e) => e.id === this.state.currentId)
+                .opciones
+            }
+          />
+          <History bitacora={this.state.bitacora} />
+        </div>
       </div>
-    </div>
-  );
+    );
+  }
 }
-}
-
 
 export default App;
--- src/components/Option.jsx
+++ src/components/Option.jsx
@@ -1,22 +1,28 @@
 import React from "react";
 
-class Option extends React.Component{
+class Option extends React.Component {
+    handleClick = (event) => {
+        this.props.counter(event.target.innerText);
+    };
 
-
-    handleClick=(event)=>{
-        this.props.counter(event.target.innerText)
-    }
-
-    
-
-    render(){
-        return(
+    render() {
+        return (
             <div className="opciones">
-               <div className="opcion"><button onClick={this.handleClick} className="botones">A</button><h2>{this.props.opciones.a}</h2></div>
-               <div className="opcion"><button onClick={this.handleClick} className="botones">B</button><h2>{this.props.opciones.b}</h2></div>
+                <div className="opcion">
+                    <button onClick={this.handleClick} className="botones">
+                        A
+                    </button>
+                    <h2>{this.props.opciones.a}</h2>
+                </div>
+                <div className="opcion">
+                    <button onClick={this.handleClick} className="botones">
+                        B
+                    </button>
+                    <h2>{this.props.opciones.b}</h2>
+                </div>
             </div>
-        )
+        );
     }
 }
 
-export default Option
+export default Option;
